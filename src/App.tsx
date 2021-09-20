c151f302673ad5fff2b8a53f5b327ca1726008f6
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-09-20 12:09:27 -0400

    Done with Adventure!

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import React from 'react'
-import monk from './icons/footer/favicon.png'
+import { Route, Switch } from 'react-router'
+import { Link } from 'react-router-dom'
+import { CategoryList } from './components/CategoryList'
+import { PhotoDetail } from './components/PhotoDetail'
+import { PhotoList } from './components/PhotoList'
 
 export function App() {
   return (
     <div>
-      <p>Hello, Dom!</p>
-      <footer>
-        <img src={monk} alt="Monk" />
-      </footer>
+      <main>
+        <header>
+          <div className="body">
+            <div className="header-container">
+              <Link to="/">
+                <h1>My Favorite Things</h1>
+              </Link>
+              <h2>A Photo Gallery by Dom Mitchell</h2>
+            </div>
+          </div>
+        </header>
+        <section className="section">
+          <div className="container">
+            <div className="panes mobile-panes">
+              <Switch>
+                <Route exact path="/">
+                  <CategoryList />
+                </Route>
+                <Route exact path="/:things">
+                  <PhotoList />
+                </Route>
+                <Route exact path="/:things/:photoIndex">
+                  <PhotoDetail />
+                </Route>
+              </Switch>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
